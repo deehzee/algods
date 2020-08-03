03d6dd2adbea82ac8d3d0a7bf47a6b8678480634
Debajyoti Nandi <deehzee@yahoo.com>
2020-08-03 13:10:58 +0530

    Update dynprog with lcs3

--- practice/dynprog.py
+++ practice/dynprog.py
@@ -199,3 +199,27 @@ def lcs2(a, b):
     return T[-1][-1]
 
 
+def lcs3(a, b, c):
+    '''Longest common subsequence of three sequences
+
+    Examples
+    --------
+    >>> lcs3([1, 2, 3], [2, 1, 3], [1, 3, 5])
+    2
+    >>> lcs3([8, 3, 2, 1, 7], [8, 2, 1, 3, 8, 10, 7], [6, 8, 3, 1, 4, 7])
+    3
+
+    '''
+    T = [[[0] * (len(c) + 1) for _ in range(len(b) + 1)]
+         for _ in range(len(a) + 1)]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            for k in range(1, len(c) + 1):
+                if a[i - 1] == b[j - 1] == c[k - 1]:
+                    T[i][j][k] = 1 + T[i - 1][j - 1][k - 1]
+                else:
+                    T[i][j][k] = max(
+                        T[i][j][k - 1], T[i][j - 1][k], T[i - 1][j][k]
+                    )
+    return T[-1][-1][-1]
+
