b97eed5b16e408f44d8f8730e966540e1653d7c6
Debajyoti Nandi <deehzee@yahoo.com>
2020-08-03 12:06:36 +0530

    Add lcs2 in dynprog

--- practice/dynprog.py
+++ practice/dynprog.py
@@ -172,3 +172,30 @@ def knapsack_wo_rep_bruteforce(capacity, weights, values, items, last):
     return value
 
 
+def lcs2(a, b):
+    '''Longest common subsequence of two arrays
+
+    Examples:
+    ---------
+    >>> lcs2([2, 7, 5], [2, 5])
+    2
+    >>> lcs2([7], [1, 2, 3, 4])
+    0
+    >>> lcs2([2, 7, 8, 3], [5, 2, 8, 7])
+    2
+
+    '''
+    # Substructure:
+    # LCS(i, j) <- lcs between A[:i], B[:j]
+    # LCS(i, j) = 1 + LCS(i-1, j-1) if A[i-1] == B[j-1]
+    #    = max(LCS(i-1, j), LCS(i, j-1)) if A[i-1] != B[j-1]
+    T = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i - 1] == b[j - 1]:
+                T[i][j] = 1 + T[i - 1][j - 1]
+            else:
+                T[i][j] = max(T[i][j - 1], T[i - 1][j])
+    return T[-1][-1]
+
+
