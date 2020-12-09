b4750544175f83694335ef0d8a8451eafd7b7e1f
Debajyoti Nandi <deehzee@yahoo.com>
2020-12-09 19:52:59 +0530

    Add tests to practice/hack../t0../p01..

--- practice/hackcoding/t0_arrays/p01_max_sliding_window.py
+++ practice/hackcoding/t0_arrays/p01_max_sliding_window.py
@@ -13,8 +13,7 @@ expected output: [2, 3, 6]
 """
 
 from collections import deque
-
-DBG = print
+import numpy as np
 
 
 # Time: O(n)
@@ -39,3 +38,49 @@ def max_in_sliding_window(arr, width):
         acc.append(arr[win[0]])
 
     return acc
+
+
+## Testing ##
+
+
+def generate_random_input(seed=None, maxlen=20, maxent=30):
+    n = np.random.randint(0, maxlen)
+    arr = np.random.randint(1, maxent + 1, n)
+    return arr
+
+
+def check(acc, arr, width):
+    ans = True
+    for i in range(len(arr) - width + 1):
+        if acc[i] != max(arr[i:i + width]):
+            ans = False
+            break
+    return ans
+
+
+def random_tests(seed=None, maxlen=20, maxent=30):
+    try:
+        n = 0
+        while True:
+            flag = False
+            arr  = generate_random_input(seed, maxlen, maxent)
+            for width in range(1, len(arr) + 1):
+                n += 1
+                acc = max_in_sliding_window(arr, width)
+                if not check(acc, arr, width):
+                    print(f'Test #{n}')
+                    print('arr:', arr)
+                    print('width:', width)
+                    flag = True
+                    break
+            if flag:
+                break
+    except KeyboardInterrupt:
+        print(f'\nPassed {n} tests.')
+
+
+if __name__ == '__main__':
+    print('Performing random tests...')
+    print('Press ^C to stop.')
+    random_tests(42, 100, 150)
+
